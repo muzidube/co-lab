105ba75611dc7132bc43765cf81ace7cca8fcbde
muzidube <muzidube1@gmail.com>
2021-10-06 18:04:31 +0100

    Finished add comment functionality

--- src/components/post/comments.js
+++ src/components/post/comments.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import PropTypes from 'prop-types';
 import { formatDistance } from 'date-fns';
 import { Link } from 'react-router-dom';
+import AddComment from "./add-comment";
 
 export default function Comments({docId, comments: allComments, posted, commentInput}) {
     const [comments, setComments] = useState(allComments);
@@ -22,6 +23,12 @@ export default function Comments({docId, comments: allComments, posted, commentI
                 ))}
                 <p className = 'text-gray-base uppercase text-xs mt-2'>{formatDistance(posted, new Date())} ago</p>
             </div>
+            <AddComment
+            docId = {docId}
+            comments = {comments}
+            setComments = {setComments}
+            commentInput = {commentInput}
+            />
         </>
     )
 }
---
+++ src/helpers/protected.route.js
@@ -0,0 +1,3 @@
+import PropTypes from 'prop-types';
+import {Route, Redirect } from 'react-router-dom';
+import * as ROUTES from '../constants/routes';
