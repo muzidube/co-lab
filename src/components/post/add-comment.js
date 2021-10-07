4138b363002c063fe9625491f1d9e1c97e3c7b54
muzidube <muzidube1@gmail.com>
2021-10-07 10:21:56 +0100

    Fixed adding comments

--- src/components/post/add-comment.js
+++ src/components/post/add-comment.js
@@ -13,35 +13,45 @@ export default function AddComment ({ docId, comments, setComments, commentInput
     const handleSubmitComment = (event) => {
         event.preventDefault()
 
-        return null;
+        setComments([{ displayName, comment }, ...comments])
+        setComment('');
+
+        return firebase
+            .firestore()
+            .collection('posts')
+            .doc(docId)
+            .update({
+                comments: FieldValue.arrayUnion({ displayName, comment})
+            })
+
     }
 
     return <div className = 'border-t border-gray-primary'>
-        <form
-            className = 'flex justify-between pl-0 pr-5'
-            method = 'POST'
-            onSubmit = {(event) => comment.length >= 1 ? handleSubmitComment(event) : event.preventDefault()}
-        >
-            <input
-                aria-label = 'Add a comment'
-                autoComplete = 'off'
-                className = 'text-sm text-gray-base w-full mr-3 py-5 px-4'
-                type = 'text'
-                name = 'add-comment'
-                placeholder = 'Add a comment...'
-                value = {comment}
-                onChange = {({ target }) => setComment(target.value)}
-                ref = {commentInput}
-            />
-            <button
-                className = {`text-sm font-bold text-blue-medium ${!comment && 'opacity-25'}`}
-                type = 'button'
-                disabled = {comment.length < 1}
-                onClick = {handleSubmitComment}
+                <form
+                    className = 'flex justify-between pl-0 pr-5'
+                    method = 'POST'
+                    onSubmit = {(event) => comment.length >= 1 ? handleSubmitComment(event) : event.preventDefault()}
                 >
-                Post
-            </button>   
-        </form>
+                    <input
+                        aria-label = 'Add a comment'
+                        autoComplete = 'off'
+                        className = 'text-sm text-gray-base w-full mr-3 py-5 px-4'
+                        type = 'text'
+                        name = 'add-comment'
+                        placeholder = 'Add a comment...'
+                        value = {comment}
+                        onChange = {({ target }) => setComment(target.value)}
+                        ref = {commentInput}
+                    />
+                    <button
+                        className = {`text-sm font-bold text-blue-medium ${!comment && 'opacity-25'}`}
+                        type = 'button'
+                        disabled = {comment.length < 1}
+                        onClick = {handleSubmitComment}
+                    >
+                        Post
+                    </button>   
+                </form>
     </div>
 
 }
